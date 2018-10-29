8648232a436312188f779edb81baac41d281b35a
mojado <s.mojado@keller-druck.ch>
2018-10-29 14:15:11 +0100

    Removed obsolete parts
    Enhanced description and removed obsoletes

--- daemon_with_access_token.py
+++ daemon_with_access_token.py
@@ -6,8 +6,8 @@
 # - Python 2.7  (Untested with Python 3.x)                                   #
 # - urllib2                                                                  #
 #                                                                            #
-# It is necessary to have a valid parameters CLIENT_SECRET + ACCESS_TOKEN    #
-# Please ask KELLER to provide a valid parameters                            #  
+# It is necessary to have a valid  ACCESS_TOKEN.                             #
+# Please ask KELLER to provide a valid ACCESS_TOKEN!                         #  
 #                                                                            #  
 # DateTime format from the API are always in UTC                             #
 # Pressure values from the API are always in bar                             #
@@ -21,8 +21,8 @@ import urllib2
 from datetime import timedelta
 import datetime
 
-# parameters client specific
-ACCESS_TOKEN = "modified_Wn74ULdcr31hL9/aEP5zKoLYHCN0MVcNmSBV9Twj5bgWuZzUDGtY/fYXkGtsVKKocEnUftMMkZlB9dFHFfWDoCDgu4wYSDuX3jXs16n+LJkpHcjDbdnObLVByQxn67yG/dczWMYRIjNd/s3qHyFAAAAMNB4dy+qNxTrW6TUVa/qk6/5esIKLyZbKG5D5eM34kpANDLOJzhcpBaOnZoNSvQgA==" # represents the user
+# client specific access token
+ACCESS_TOKEN = "___modified___sVKKoibnUftMMkZlB9dFHFfWDoCDgu4wYSDvX3jXs16n+LJkpHcjDbdnObLVByQxn67yG/dczWMYrIjNd/s3qHyAAAAAMNB4dy+qNxTrW6TUVa/qk6/5esIKLuZbKG5D5eM34kpANDLOJzhcpBaOnZoNSvQgA==" # represents the user
 
 def turn_on_logging_of_imported_libraries():
     """ Use this to get more log information from the ADAL library and the urllib2
@@ -32,7 +32,7 @@ def turn_on_logging_of_imported_libraries():
 
 def get_data(_endpoint, _access_token):
     """
-    With the correct Bearer Token and the correct Access Token one can get data from the API.
+    With the correct Access Token one can get data from the API.
     The API specification can be seen here: 
     https://api.kolibricloud.ch/swagger/index.html?url=/swagger/v1/swagger.json
     """
@@ -43,8 +43,12 @@ def get_data(_endpoint, _access_token):
     try:
         response = urllib2.urlopen(req) 
     except urllib2.HTTPError, e: print e.headers
-    html = response.read()
-    json_obj = json.loads(html)
+    try:
+        html = response.read()
+        json_obj = json.loads(html)
+    except UnboundLocalError:
+        print("Could not find data in "+_endpoint+"\n")
+        json_obj = ""
     return json_obj
 
 def get_measurementDefinitionId_LookUpTable():
@@ -77,8 +81,26 @@ def get_measurementDefinitionId_LookUpTable():
         26: "SDI12 CH7",
         27: "SDI12 CH8",
         28: "SDI12 CH9",
-        29: "SDI12 CH10"
-        }
+        29: "SDI12 CH10",
+        30: "TOB1 (2)",
+        31: "TOB1 (3)",
+        32: "TOB1 (4)",
+        33: "TOB1 (5)",
+        34: "E",
+        35: "F",
+        36: "G",
+        37: "mH20 (PBaro)",
+        38: "mH20 (P1-P2)",
+        39: "mH20 (P1-P3)",
+        40: "mH20 (P1-P4)",
+        41: "mH20 (P1-P5)",
+        42: "Conductivity Tc (2)",
+        43: "Conductivity Tc (3)",
+        44: "T (Conductivity) (2)",
+        45: "T (Conductivity) (3)",
+        46: "P2 (2)",
+        47: "TOB2 (2)"
+    }
     return measurementDefinitionId_LookUpTable
 
 def get_data_measurements_from_timespan(timespan_in_hours, measurementDefinitionId, deviceId, _access_token):
@@ -100,20 +122,19 @@ data1 = get_data(endpoint, ACCESS_TOKEN)
 print(json.dumps(data1, indent=2))
 print("----------------------------------------------------"+"\n")
 
-my_device = data1["devices"][0]["id"]
-# or better use the specific device id my_device = 1234  # must be a device id that is in the set of accessible devices
+my_device = data1["devices"][0]["id"] # or better use the specific device id my_device = 1234  # must be a device id that is in the set of accessible devices
 endpoint = "/v1/Devices/"+str(my_device)
 data2 = get_data(endpoint, ACCESS_TOKEN)
 print("Here are information of device "+str(my_device)+":")
 print("(If you do not own #"+str(my_device)+" this will break here.)")
 print(json.dumps(data2, indent=2))
 print("----------------------------------------------------"+"\n")
-#print('Last measurement time was: '+data2['lastMeasurementTransmissionDateTime'])
+print('Last measurement time was: '+data2['lastMeasurementTransmissionDateTime'])
 
 
-# data3 is the JSON data from device "my_device" of the channel with the id 8 which is TBaro in [°C] of the last 6 hours
-measurementDefinitionId = 8 #  is TBaro / See get_measurementDefinitionId_LookUpTable()
-data3 = get_data_measurements_from_timespan(6, measurementDefinitionId, my_device, ACCESS_TOKEN)
+# data3 is the JSON data from device "my_device" of the channel with the id 8 which is TBaro in [° C] of the last 12 hours
+measurementDefinitionId = 8 #  is TBaro (Air Temperature) / See get_measurementDefinitionId_LookUpTable()
+data3 = get_data_measurements_from_timespan(12, measurementDefinitionId, my_device, ACCESS_TOKEN)
 measurementDefinitionIds = get_measurementDefinitionId_LookUpTable()
 print("Measurements of " + measurementDefinitionIds[measurementDefinitionId]+" : ")
 print(json.dumps(data3, indent=2))
@@ -127,5 +148,5 @@ for each_device in data1['devices']:
     all_channels_of_this_device = []
     for each_channel in data['measurementDefinitions']:
         all_channels_of_this_device.append(str(each_channel['id'])+":"+each_channel['name'])
-    all_channels_of_this_device = [str(all_channels_of_this_device[x]) for x in range(len(all_channels_of_this_device))] #just stringify more beautiful
+    all_channels_of_this_device = [str(all_channels_of_this_device[x]) for x in range(len(all_channels_of_this_device))] #just prettify the texts
     print("#"+str(each_device['id'])+" has measurement channels: "+str(all_channels_of_this_device))
